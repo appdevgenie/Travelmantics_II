f06c580379fdc1d46bae6ee5b2a61f40a5fe7b6e
appdevgenie <appdevgenie@gmail.com>
2019-08-01 18:44:19 +0200

    add main user menu

--- app/src/main/java/com/appdevgenie/travelmanticsii/activities/UserActivity.java
+++ app/src/main/java/com/appdevgenie/travelmanticsii/activities/UserActivity.java
@@ -1,9 +1,14 @@
 package com.appdevgenie.travelmanticsii.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
 
 import com.appdevgenie.travelmanticsii.R;
 
@@ -15,5 +20,29 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.user_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int selectedItem = item.getItemId();
+
+        switch (selectedItem) {
+            case R.id.add_menu:
+                Intent intent = new Intent(UserActivity.this, AdminActivity.class);
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
